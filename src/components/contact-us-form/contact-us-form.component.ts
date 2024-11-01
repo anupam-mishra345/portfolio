54e56afacf228019f1e11d24461f610804efd3e6
Anupam <anupam@ajapro.com>
2024-11-01 15:19:56 +0530

    added email api integration

--- src/components/contact-us-form/contact-us-form.component.ts
+++ src/components/contact-us-form/contact-us-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { ThemeService } from 'src/services/theme.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailService } from 'src/services/email.service';
 
 @Component({
   selector: 'app-contact-us-form',
@@ -13,7 +14,11 @@ export class ContactUsFormComponent {
   isDarkMode: boolean = false;
   @Output() cancelForm = new EventEmitter();
 
-  constructor(private themeService: ThemeService, private fb: FormBuilder) {
+  constructor(
+    private themeService: ThemeService,
+    private fb: FormBuilder,
+    private emailService: EmailService
+  ) {
     this.form = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(2)]],
       lastName: [''],
@@ -37,8 +42,75 @@ export class ContactUsFormComponent {
   onSubmit() {
     if (this.form.valid) {
       console.log('Form Submitted', this.form.value);
+      this.sendContactEmail();
     } else {
       console.log('Form is invalid');
     }
+  }
+
+  sendContactEmail() {
+    this.emailService
+      .sendEmail(
+        'anupam.mishra3451@gmail.com',
+        'Portfolio Query',
+        this.emailTemplateUI()
+      )
+      .then((response) => {
+        console.log('Email sent successfully:', response);
+      })
+      .catch((error) => {
+        console.error('Failed to send email:', error);
+      });
+  }
+
+  emailTemplateUI() {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Email Template</title>
+    </head>
+    <body style="font-family: Arial, sans-serif; margin: 0; padding: 20px 0 0 0; background-color: #f4f4f4;">
+
+      <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 8px; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1); overflow: hidden;">
+        
+        <!-- Header -->
+        <div style="background-color: #4caf50; padding: 15px; text-align: center; color: #ffffff; font-size: 24px;">
+          New Message Received
+        </div>
+        
+        <!-- Content -->
+        <div style="padding: 20px; color: #333333;font-size: 14px">
+          <p style="margin: 8px 0;">
+            <span style="font-weight: bold; color: #555555;">Name:</span>
+            <span style="color: #333333;">${this.form.value.firstName} ${this.form.value.lastName}</span>
+          </p>
+          <p style="margin: 8px 0;">
+            <span style="font-weight: bold; color: #555555;">Email:</span>
+            <span style="color: #333333;">${this.form.value.email}</span>
+          </p>
+          <p style="margin: 8px 0;">
+            <span style="font-weight: bold; color: #555555;">Mobile Number:</span>
+            <span style="color: #333333;">${this.form.value.mobile}</span>
+          </p>
+          <p style="margin: 8px 0;">
+            <span style="font-weight: bold; color: #555555;">Message:</span>
+          </p>
+          <p style="color: #333333;">${this.form.value.message}</p>
+        </div>
+        
+        <!-- Footer -->
+        <div style="background-color: #f4f4f4; padding: 10px; text-align: center; font-size: 12px; color: #777777;">
+          This is an automated email. Please do not reply.
+        </div>
+        
+      </div>
+
+    </body>
+    </html>
+
+    `;
   }
 }
---
+++ src/services/email.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+import { environment } from 'src/environment';
+// import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmailService {
+  private brevoApiUrl = 'https://api.brevo.com/v3/smtp/email';
+
+  apiKey = environment.apiKey;
+
+  async sendEmail(
+    toEmail: string,
+    subject: string,
+    content: string
+  ): Promise<any> {
+    const emailData = {
+      sender: { email: 'anupam.mishra345@gmail.com' },
+      to: [{ email: toEmail }],
+      subject: subject,
+      htmlContent: content,
+    };
+
+    try {
+      const response = await axios.post(this.brevoApiUrl, emailData, {
+        headers: { 'api-key': this.apiKey, 'Content-Type': 'application/json' },
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error sending email:', error);
+      throw error;
+    }
+  }
+}
