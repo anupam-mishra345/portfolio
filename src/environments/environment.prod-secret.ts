3df3913424000f75e844b8b004652c6af7c904aa
anupam <anupam@anupams-MacBook-Air.local>
2025-09-13 00:40:46 +0530

    added env file for email js

---
+++ src/environments/environment.prod-secret.ts
@@ -0,0 +1,8 @@
+          
+
+export const enviroment = {
+  production: true,
+  emailServiceId: 'service_c3my6or',
+  emailTemplateId: 'template_350pbyp',
+  emailUserId: 'D7AuBN7nhGp5NMcL0',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+          
+
+export const enviroment = {
+  production: false,
+  emailServiceId: 'service_c3my6or',
+  emailTemplateId: 'template_350pbyp',
+  emailUserId: 'D7AuBN7nhGp5NMcL0',
+};
